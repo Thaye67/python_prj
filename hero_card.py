bb4ee39840bf7e91d51d7e3ab2182e531b890902
Thaye67 <your.email@example.com>
2026-08-28 22:23:18 -0700

    initialize repo

---
+++ hero_card.py
@@ -0,0 +1,6 @@
+name = input("enter Hero Name: ")
+power = input("enter Superpower: ")
+city = input("enter Home City: ")
+
+print("--- HERO FILE CREATED ---")
+print(name,"guards",city,"with the power of",power)
